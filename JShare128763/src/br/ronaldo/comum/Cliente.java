4946d857dba8c6565c26cf54e9169926a06eb967
Ronaldo Zuchi <faculdadetads@gmail.com>
2016-04-10 10:18:42 -0300

    Atribuido as variaveis nome, ip e porta e seus métodos get e set.

--- JShare128763/src/br/ronaldo/comum/Cliente.java
+++ JShare128763/src/br/ronaldo/comum/Cliente.java
@@ -1,5 +1,32 @@
 package br.ronaldo.comum;
 
-public class Cliente {
+import java.io.Serializable;
+
+public class Cliente implements Serializable{
+
+	private static final long serialVersionUID = 3623490832380407912L;
+	
+	private String nome;
+	private String ip;
+	private int porta;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public int getPorta() {
+		return porta;
+	}
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
 
 }

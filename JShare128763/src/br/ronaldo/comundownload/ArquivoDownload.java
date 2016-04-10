35ab44da16ce6a195016c67ce923987cc4179d07
Ronaldo Zuchi <faculdadetads@gmail.com>
2016-04-10 10:24:03 -0300

    Informado na classe ArquivoDownload os atributos nome do arquivo e tamanho do arquivo, criado também get e set para os atributos.

--- JShare128763/src/br/ronaldo/comundownload/ArquivoDownload.java
+++ JShare128763/src/br/ronaldo/comundownload/ArquivoDownload.java
@@ -1,5 +1,30 @@
 package br.ronaldo.comundownload;
 
-public class ArquivoDownload {
+import java.io.Serializable;
+
+public class ArquivoDownload implements Serializable{
+
+	private static final long serialVersionUID = -455063189151041498L;
+	
+	private String nomeArquivo;
+	private long tamanhoArquivo;
+	
+	public String getNomeArquivo() {
+		return nomeArquivo;
+	}
+	public void setNomeArquivo(String nomeArquivo) {
+		this.nomeArquivo = nomeArquivo;
+	}
+	public long getTamanhoArquivo() {
+		return tamanhoArquivo;
+	}
+	public void setTamanhoArquivo(long tamanhoArquivo) {
+		this.tamanhoArquivo = tamanhoArquivo;
+	}
+	
+	@Override
+	public String toString() {
+		return nomeArquivo + " - " + tamanhoArquivo;
+	}
 
 }

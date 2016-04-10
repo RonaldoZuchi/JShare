c663f0314f641a2daf881af6d14e390dd28a4d04
Ronaldo Zuchi <faculdadetads@gmail.com>
2016-04-10 14:35:47 -0300

    Informado na classe DiretorioDownload o atributo nome get e set deste atributo.

--- JShare128763/src/br/ronaldo/comundownload/DiretorioDownload.java
+++ JShare128763/src/br/ronaldo/comundownload/DiretorioDownload.java
@@ -2,4 +2,14 @@ package br.ronaldo.comundownload;
 
 public class DiretorioDownload {
 
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
